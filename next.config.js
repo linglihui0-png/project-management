f1a89dcda8d2c2ee87b62ff2ed78bc627f28bb29
linglihui0-png <linglihui0@gmail.com>
2026-04-14 16:02:34 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  eslint: {
+    // 忽略格式校验错误
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // 忽略类型校验错误
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
